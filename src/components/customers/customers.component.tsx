8e17af41be06c79aa3b568c9fdb595df1c17f14d
Eden Azulay <edenaz@mta.ac.il>
2023-05-28 22:19:01 +0300

    get_all_customers table

--- src/components/customers/customers.component.tsx
+++ src/components/customers/customers.component.tsx
@@ -1,31 +1,73 @@
-import React, {useEffect, useState} from 'react'
-import '../dashboard-widgets/widgets.style.css'
-import './customers.style.css'
-import CustomerDelegate from './customer-delegate/customer-delegate.component'
-import {devCustomers} from "./dev-data";
-import SearchField from "../search-field/search-field.component";
-import NavigationButtonComponent from "../navigation-button/navigation-button.component";
+import React, { useEffect, useState } from 'react';
+import '../dashboard-widgets/widgets.style.css';
+import './customers.style.css';
+import CustomerDelegate from './customer-delegate/customer-delegate.component';
+
+import SearchField from '../search-field/search-field.component';
+import NavigationButtonComponent from '../navigation-button/navigation-button.component';
+import axios from 'axios';
 
 interface ICustomerProps {
-    className: string,
-    header: string,
-    description: string
+    className: string;
+    header: string;
+    description: string;
 }
 
-export default function Customers({className, header, description}: ICustomerProps) {
+interface ICustomer {
+    name: string;
+    phoneNumber: string;
+    email: string;
+    address: string;
+    orders: {
+        id: string;
+        dueDate: number;
+        totalCost: number;
+    }[];
+}
 
-    const [customers, setCustomers] = useState(devCustomers);
-    const [filteredCustomers, setFilteredCustomers] = useState(customers);
+export default function Customers({ className, header, description }: ICustomerProps) {
+    const [customers, setCustomers] = useState<ICustomer[]>([]);
+    const [filteredCustomers, setFilteredCustomers] = useState<ICustomer[]>([]);
     const [searchString, setSearchString] = useState('');
+
+    useEffect(() => {
+        fetchCustomerDetails();
+    }, []);
+
+    const fetchCustomerDetails = async () => {
+        try {
+            const response = await axios.get('https://5wcgnzy0bg.execute-api.us-east-1.amazonaws.com/dev/customers');
+            const data = response.data;
+
+            const formattedCustomers = data.map((customer: any) => {
+                return {
+                    name: customer.name,
+                    phoneNumber: customer.phoneNumber,
+                    email: customer.email,
+                    address: customer.address,
+                    orders: customer.orders.map((order: any) => {
+                        return {
+                            id: order.id,
+                            dueDate: order.dueDate,
+                            totalCost: order.totalCost,
+                        };
+                    }),
+                };
+            });
+            setCustomers(formattedCustomers);
+            setFilteredCustomers(formattedCustomers);
+        } catch (error) {
+            console.error('Error fetching customer details:', error);
+        }
+    };
 
     useEffect(() => {
         const filtered = customers.filter((customer) => {
             const name = customer.name.toLowerCase();
-            console.log(name, searchString, name.includes(searchString))
             return name.includes(searchString);
-        })
-        setFilteredCustomers(filtered)
-    }, [customers, searchString])
+        });
+        setFilteredCustomers(filtered);
+    }, [customers, searchString]);
 
     return (
         <div className={`dashboard-widget-container customers-widget ${className}`}>
@@ -36,11 +78,12 @@ export default function Customers({className, header, description}: ICustomerPro
                         <span className="widget-title-text-secondary">{description}</span>
                     </div>
                     <div className="customers-header-find-container">
-                        <SearchField placeholder="Find" onChangeHandler={
-                            (event: any) => {
-                                setSearchString(event.target.value.toLowerCase())
-                            }
-                        }/>
+                        <SearchField
+                            placeholder="Find"
+                            onChangeHandler={(event: React.ChangeEvent<HTMLInputElement>) => {
+                                setSearchString(event.target.value.toLowerCase());
+                            }}
+                        />
                     </div>
                 </div>
 
@@ -58,14 +101,12 @@ export default function Customers({className, header, description}: ICustomerPro
             </div>
             <div className="customers-list-container">
                 <div className="customers-list">
-                    {
-                        filteredCustomers.map((customer) => {
-                            return <CustomerDelegate key={customer.name} data={customer}/>
-                        })
-                    }
+                    {filteredCustomers.map((customer) => {
+                        return <CustomerDelegate key={customer.name} data={customer} />;
+                    })}
                 </div>
             </div>
-            <NavigationButtonComponent to="/customers/new" text="Add Customer"/>
+            <NavigationButtonComponent to="/customers/new" text="Add Customer" />
         </div>
-    )
+    );
 }
